306709fae5e05b579db0c18688c1b82e7af9d7ba
MadhuA24 <01fe24bec287@kletech.ac.in>
2026-04-15 09:32:56 +0530

    Network Routing System

--- DSA_3.c
+++ DSA_3.c
@@ -1,179 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct node
-{
+//Network Routing System
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
     int data;
-    struct node *left;
-    struct node *right;
-}*node;
-node insert(node root);
-node delete_node(node root,int data);
-void preorder(node root);
-void inorder(node root);
-void postorder(node root);
-int main()
-{
-   node root=NULL;
-   int choice,data;
-   while(1)
-   {
-       printf("\n enter the choice :");
-       scanf("%d",&choice);
-       switch(choice)
-       {
-           case 0:exit(0);break;
-           case 1:root=insert(root);
-           break;
-           case 2:
-               printf("enter data :\n");
-               scanf("%d",&data);
-               root=delete_node(root,data);
-           break;
-           case 3:preorder(root);
-           break;
-           case 4:inorder(root);
-           break;
-           case 5:postorder(root);
-           break;
-           default:
-            printf("invalid choice:\n");
-            break;
-       }
-   }
-   return 0;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = value;
+    node->left = node->right = NULL;
+    return node;
 }
-node create()
-{
-    node newnode=(node)malloc(sizeof(struct node));
-    if(newnode==NULL)
-    {
-        printf("the memory is not allocated:\n");
-        return NULL;
-    }
-    printf("enter the data :\n");
-    scanf("%d",&newnode->data);
-    newnode->left=newnode->right=NULL;
-    return newnode;
-}
-node insert(node root)
-{
-    node newnode=create();
-    node cur=root,parent=NULL;
-    if(root==NULL)
-    {
-        root=newnode;
-    }
-    else{
-        while(cur!=NULL)
-        {
-            parent=cur;
-            if(newnode->data<cur->data)
-            {
-                cur=cur->left;
-            }
-            else{
-                cur=cur->right;
-            }
-        }
-        if(newnode->data<parent->data)
-        {
-            parent->left=newnode;
-        }
-        else{
-            parent->right=newnode;
-        }
-    }
+
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
     return root;
 }
-void preorder(node root)
-{
-    if(root!=NULL)
-    {
-        printf("%d ",root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
-void inorder(node root)
-{
-    if(root!=NULL)
-    {
+
+void inorder(struct Node* root) {
+    if (root != NULL) {
         inorder(root->left);
-        printf("%d ",root->data);
+        printf("%d ", root->data);
         inorder(root->right);
     }
 }
-void postorder(node root)
-{
-    if(root!=NULL)
-    {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ",root->data);
-    }
-}
-node delete_node(node root,int data)
-{
-    if(root==NULL)
-    {
-        printf("tree is empty :\n");
-        return root;
-    }
-    else{
-        node cur=root,parent=NULL;
-        while(cur!=NULL&&data!=cur->data)
-        {
-            parent=cur;
-            if(data<cur->data)
-            {
-                cur=cur->left;
-            }
-            else{
-                cur=cur->right;
-            }
-        }
 
-        if(cur==NULL)
-        {
-            printf("the element is not found:\n");
-            return root;
-        }
-        //node has two children
-        if(cur->left!=NULL&&cur->right!=NULL)
-        {
-            node sc=cur->right;
-            node sparent=cur;
-        while(sc->left!=NULL)
-        {
-            sparent=sc;
-            sc=sc->left;
-        }
-        cur->data=sc->data;
-        cur=sc;
-        parent=sparent;
-      }
-      //case 2 one child OR No child
-      node child;
-      if(cur->left!=NULL)
-      {
-          child=cur->left;
-      }
-      else{
-        child=cur->right;
-      }
-      if(parent==NULL)
-      {
-          free(cur);
-          return child;
-      }
-      if(parent->left==cur)
-      {
-          parent->left=child;
-      }
-      else{
-        parent->right=child;
-      }
-      free(cur);
+int main() {
+    int n, i, x, newID;
+    struct Node* root = NULL;
+
+    if (scanf("%d", &n) != 1 || n == -1 || n < 1 || n > 100) {
+        printf("Invalid input");
+        return 0;
     }
-    return root;
+
+    for (i = 0; i < n; i++) {
+        if (scanf("%d", &x) != 1 || x == -1) {
+            printf("Invalid input");
+            return 0;
+        }
+        root = insert(root, x);
+    }
+
+    if (scanf("%d", &newID) != 1 || newID == -1) {
+        printf("Invalid input");
+        return 0;
+    }
+
+    root = insert(root, newID);
+
+    inorder(root);
+
+    return 0;
 }
